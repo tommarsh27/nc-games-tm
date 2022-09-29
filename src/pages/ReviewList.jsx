d1b6ec0671dd8f4bf70c973cf0c1aed8aedc74fe
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-29 15:26:55 +0100

    renaming

--- src/pages/ReviewList.jsx
+++ src/pages/ReviewList.jsx
@@ -4,12 +4,10 @@ import Loading from "../components/Loading";
 import ReviewCard from "../components/ReviewCard";
 import axios from "axios";
 import CategoryBar from "../components/CategoryBar";
-
 const ReviewList = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [reviews, setReviews] = useState([]);
   const {slug} = useParams();
-
   useEffect(() => {
     setIsLoading(true);
     
@@ -20,23 +18,18 @@ const ReviewList = () => {
         setIsLoading(false);
       });
   }, [slug]);
-
   return (
     <section>
         <CategoryBar />
       <Loading isLoading={isLoading}>
         <h2>Reviews</h2>
         <ul className="ul">
-          {reviews.map(review => 
-            <ReviewCard 
-              key={review.review_id} 
-              review={review} 
-            />)}
+          {reviews.map((review) => {
+            return <ReviewCard key={review.review_id} review={review} />;
+          })}
         </ul>
       </Loading>
     </section>
   );
 };
-
 export default ReviewList;
-
