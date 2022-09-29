5da85430db7c6af2b1288e18c826eacb8724deca
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-29 16:47:19 +0100

    refactored api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const gamesApi = axios.create({
+  baseURL: 'https://be-games-project-tm.herokuapp.com/api'
+})
+
 export const getReviews = (category) => {
-    return axios
-    .get('https://be-games-project-tm.herokuapp.com/api/reviews', {params: {category}})
+    return gamesApi
+    .get('/reviews', {params: {category}})
     .then(({ data }) => {
       return data.reviews;
     });
