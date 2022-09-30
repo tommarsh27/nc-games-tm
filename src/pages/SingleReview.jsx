62e2c82c8668599c3a8e1975e63b0a052800d989
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-30 12:39:55 +0100

    fixed refactoring errors

--- src/pages/SingleReview.jsx
+++ src/pages/SingleReview.jsx
@@ -1,35 +1,31 @@
-import axios from "axios";
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import ReviewCard from "../components/ReviewCard";
-
+import { getSingleReview } from "../utils/api";
 
 const SingleReview = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [review, setReview] = useState({});
-    const {review_id} = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [review, setReview] = useState({});
+  const { review_id } = useParams();
 
-    useEffect(() => {
-        setIsLoading(true);
-        axios
-        .get(`https://be-games-project-tm.herokuapp.com/api/reviews/${review_id}`)
-        .then(({data}) => {
-            const {review} = data;
-            setReview(review);
-            setIsLoading(false);
-        })
-    }, [review_id]);
+  useEffect(() => {
+    setIsLoading(true);
+    getSingleReview(review_id)
+    .then((review) => {
+        setReview(review);
+        setIsLoading(false);
+    })
+  }, [review_id]);
 
-    return (
-        <section>
-            <Loading isLoading={isLoading}>
-                <ReviewCard review={review} />
-            </Loading>
-        </section>
-    )
-}
+  return (
+    <section>
+      <Loading isLoading={isLoading}>
+        <ReviewCard review={review} />
+      </Loading>
+    </section>
+  );
+};
 
-export default SingleReview
+export default SingleReview;
